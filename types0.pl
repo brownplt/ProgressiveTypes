a547f9f57b631477cf9b57a8e775c18fd4f81871
joseph <joe@cs.brown.edu>
2012-02-16 10:45:39 -0500

    unicode prettiness, more units

--- types0.pl
+++ types0.pl
@@ -1,5 +1,5 @@
 :- module(types0, [
-  type/3
+  type/4
 ]).
 
 isType(nzInt).
@@ -8,40 +8,33 @@ isType(zero).
 isType(union(S,T)) :- isType(S), isType(T).
 isType(arrow(S,T)) :- isType(S), isType(T).
 
-allowed(T) :- isType(T).
-allowed(erriplusrightfloat).
-allowed(erriplusleftfloat).
-allowed(errDivByZeroWithIntNumerator).
-allowed(errDivByZeroWithFloatNumerator).
+type(_, _, nzIntConst, nzInt).
+type(_, _, nzFltConst, nzniFlt).
+type(_, _, zeroConst, zero).
+type(Ω, Γ, if(Test, Then, Else), Tau) :-
+  type(Ω, Γ, Test, nzInt),
+  type(Ω, Γ, Then, Tau),
+  type(Ω, Γ, Else, Tau).
 
-type(_, nzIntConst, nzInt).
-type(_, nzFltConst, nzniFlt).
-type(_, zeroConst, zero).
-type(TEnv, if(Test, Then, Else), Tau) :-
-  type(TEnv,Test,nzInt),
-  type(TEnv,Then,Tau),
-  type(TEnv,Else,Tau).
-
-type(Env, subsume(E, T), T) :-
-  type(Env, E, S),
+type(Ω, Env, subsume(E, T), T) :-
+  type(Ω, Env, E, S),
   subtype(S, T).
 
-type([bind(V,T)|_], var(V), T).
-type([bind(_,_)|TEnvRest], var(V), T) :-
-  type(TEnvRest, var(V), T).
+type(_, [bind(V,T)|_], var(V), T).
+type(Ω, [bind(_,_)|Γ], var(V), T) :-
+  type(Ω, Γ, var(V), T).
 
-type(TEnv, fun(Arg, ArgTyp, Body), arrow(ArgTyp, T2)) :-
-  type([bind(Arg, ArgTyp)|TEnv], Body, T2).
+type(Ω, Γ, λ(X, S, Body), arrow(S, T)) :-
+  type(Ω, [bind(X, S)|Γ], Body, T).
 
-type(TEnv, app(Fun, Arg), T2) :-
-  type(TEnv, Fun, arrow(T1, T2)),
-  type(TEnv, Arg, T1).
+type(Ω, Γ, app(Fun, Arg), T2) :-
+  type(Ω, Γ, Fun, arrow(T1, T2)),
+  type(Ω, Γ, Arg, T1).
 
-type(TEnv, op(Op, E1, E2), T) :-
-  type(TEnv, E1, T1),
-  type(TEnv, E2, T2),
-  delta(Op, T1, T2, T),
-  allowed(T).
+type(Ω, Γ, op(Op, E1, E2), T) :-
+  type(Ω, Γ, E1, T1),
+  type(Ω, Γ, E2, T2),
+  delta(Op, T1, T2, Ω, T).
 
 wftype(T) :- safe_canonical(T,T).
 
@@ -77,51 +70,66 @@ subtype(arrow(S1, T1), arrow(S2, T2)) :-
   (subtype(S2, S1), subtype(T1, T2)).
 
 % Delta distributes over unions
-delta(Op, union(S1, S2), T2, union(TS1, TS2)) :-
-  delta(Op, S1, T2, TS1),
-  delta(Op, S2, T2, TS2).
+delta(Op, union(S1, S2), T2, Ω, union(TS1, TS2)) :-
+  delta(Op, S1, T2, Ω, TS1),
+  delta(Op, S2, T2, Ω, TS2).
 
-delta(Op, T2, union(S1, S2), union(TS1, TS2)) :-
-  delta(Op, T2, S1, TS1),
-  delta(Op, T2, S2, TS2).
+delta(Op, T2, union(S1, S2), Ω, union(TS1, TS2)) :-
+  delta(Op, T2, S1, Ω, TS1),
+  delta(Op, T2, S2, Ω, TS2).
 
-delta(plus, zero, zero, zero).
-delta(plus, zero, nzniFlt, nzniFlt).
-delta(plus, zero, nzInt, nzInt).
-delta(plus, nzniFlt, zero, nzniFlt).
-delta(plus, nzniFlt, nzniFlt, union(nzInt, nzFloat)).
-delta(plus, nzniFlt, nzInt, nzniFlt).
-delta(plus, nzInt, zero, nzInt).
-delta(plus, nzInt, nzniFlt, nzniFlt).
-delta(plus, nzInt, nzInt, nzInt).
+delta(_, ⊥ , _, _, ⊥ ).
+delta(_, _, ⊥ , _, ⊥ ).
 
-delta(plus, arrow(_, _), _, errPlusLambdaLeft).
-delta(plus, T, arrow(_, _), errPlusLambdaRight) :-
+delta(plus, zero, zero, _, zero).
+delta(plus, zero, nzniFlt, _, nzniFlt).
+delta(plus, zero, nzInt, _, nzInt).
+delta(plus, nzniFlt, zero, _, nzniFlt).
+delta(plus, nzniFlt, nzniFlt, _, union(nzInt, nzFloat)).
+delta(plus, nzniFlt, nzInt, _, nzniFlt).
+delta(plus, nzInt, zero, _, nzInt).
+delta(plus, nzInt, nzniFlt, _, nzniFlt).
+delta(plus, nzInt, nzInt, _, nzInt).
+
+delta(plus, arrow(_, _), _, Ω, ⊥ ) :-
+  member(errPlusLambdaLeft, Ω).
+delta(plus, T, arrow(_, _), Ω, ⊥ ) :-
+  member(errPlusLambdaRight, Ω),
   subtype(T, union(nzInt, nzniFlt, zero)).
 
-delta(iplus, zero, zero, zero).
-delta(iplus, zero, nzniFlt, errIPlusRightFloatWithZeroLeft).
-delta(iplus, zero, nzInt, nzInt).
-delta(iplus, nzniFlt, zero, errIPlusLeftFloatWithZeroRight).
-delta(iplus, nzniFlt, nzniFlt, errIPlusBothFloat).
-delta(iplus, nzniFlt, nzInt, errIPlusLeftFloatWithIntright).
-delta(iplus, nzInt, zero, nzInt).
-delta(iplus, nzInt, nzniFlt, errIPlusRightFloatWithIntLeft).
-delta(iplus, nzInt, nzInt, nzInt).
+delta(iplus, zero, zero, _, zero).
+delta(iplus, zero, nzniFlt, Ω, ⊥ ) :-
+  member(errIPlusRightFloatWithZeroLeft, Ω).
+delta(iplus, zero, nzInt, _, nzInt).
+delta(iplus, nzniFlt, zero, Ω, ⊥ ) :-
+  member(errIPlusLeftFloatWithZeroRight, Ω).
+delta(iplus, nzniFlt, nzniFlt, Ω, ⊥ ) :-
+  member(errIPlusBothFloat, Ω).
+delta(iplus, nzniFlt, nzInt, Ω, ⊥ ) :-
+  member(errIPlusLeftFloatWithIntright, Ω).
+delta(iplus, nzInt, zero, _, nzInt).
+delta(iplus, nzInt, nzniFlt, Ω, ⊥ ) :-
+  member(errIPlusRightFloatWithIntLeft, Ω).
+delta(iplus, nzInt, nzInt, _, nzInt).
 
-delta(iplus, arrow(_, _), _, errIPlusLambdaLeft).
-delta(iplus, T, arrow(_, _), errIPlusLambdaRight) :-
+delta(iplus, arrow(_, _), _, Ω, errIPlusLambdaLeft) :-
+  member(errIPlusLambdaLeft, Ω).
+delta(iplus, T, arrow(_, _), Ω, errIPlusLambdaRight) :-
+  member(errIPlusLambdaRight, Ω),
   subtype(T, union(nzInt, zero)).
 
-delta(div, zero, zero, errDivByZeroWithZeroNumerator).
-delta(div, zero, nzniFlt, zero).
-delta(div, zero, nzInt, zero).
-delta(div, nzniFlt, zero, errDivByZeroWithFloatNumerator).
-delta(div, nzniFlt, nzniFlt, union(nzInt, nzniFlt)).
-delta(div, nzniFlt, nzInt, nzniFlt).
-delta(div, nzInt, zero, errDivByZeroWithIntNumerator).
-delta(div, nzInt, nzniFlt, union(nzniFlt, nzInt)).
-delta(div, nzInt, nzInt, union(nzniFlt, nzInt)).
+delta(div, zero, zero, Ω, ⊥ ) :-
+  member(errDivByZeroWithZeroNumerator, Ω).
+delta(div, zero, nzniFlt, _, zero).
+delta(div, zero, nzInt, _, zero).
+delta(div, nzniFlt, zero, Ω, ⊥ ) :-
+  member(errDivByZeroWithFloatNumerator, Ω).
+delta(div, nzniFlt, nzniFlt, _, union(nzInt, nzniFlt)).
+delta(div, nzniFlt, nzInt, _, nzniFlt).
+delta(div, nzInt, zero, Ω, ⊥ ) :-
+  member(errDivByZeroWithIntNumerator, Ω).
+delta(div, nzInt, nzniFlt, _, union(nzniFlt, nzInt)).
+delta(div, nzInt, nzInt, _, union(nzniFlt, nzInt)).
 
 delta(div, arrow(_, _), _, errIPlusLambdaLeft).
 delta(div, T, arrow(_, _), errIPlusLambdaRight) :-
--- types0tests.pl
+++ types0tests.pl
@@ -2,42 +2,53 @@
 :- use_module('types0', except([])).
 
 test(divZero, [nondet]) :-
-  type([], op(div, nzIntConst, zeroConst), errDivByZeroWithIntNumerator).
+  type([errDivByZeroWithIntNumerator], [], op(div, nzIntConst, zeroConst), ⊥ ).
+
+test(divZero, [fail]) :-
+  type([], [], op(div, nzIntConst, zeroConst), ⊥ ).
 
 test(divInt, [nondet]) :-
-  type([], op(div, nzIntConst, nzIntConst), union(nzniFlt, nzInt)).
+  type([], [], op(div, nzIntConst, nzIntConst), union(nzniFlt, nzInt)).
 
 test(divFlt, [nondet]) :-
-  type([], op(div, nzIntConst, nzFltConst), union(nzniFlt, nzInt)).
+  type([], [], op(div, nzIntConst, nzFltConst), union(nzniFlt, nzInt)).
 
 test(divZeroTop, [nondet]) :-
-  type([], op(div, zeroConst, nzIntConst), zero).
+  type([], [], op(div, zeroConst, nzIntConst), zero).
 
 test(iplusIZ, [nondet]) :-
-  type([], op(iplus, nzIntConst, zeroConst), nzInt).
+  type([], [], op(iplus, nzIntConst, zeroConst), nzInt).
 
 test(plusIZ, [nondet]) :-
-  type([], op(plus, nzIntConst, zeroConst), nzInt).
+  type([], [], op(plus, nzIntConst, zeroConst), nzInt).
 
 test(plusFZ, [nondet]) :-
-  type([], op(plus, nzFltConst, zeroConst), nzniFlt).
+  type([], [], op(plus, nzFltConst, zeroConst), nzniFlt).
 
 test(plusZI, [nondet]) :-
-  type([], op(iplus, zeroConst, nzIntConst), nzInt).
+  type([], [], op(iplus, zeroConst, nzIntConst), nzInt).
 
 test(funDivIX, [nondet]) :-
-  type([], fun(x, union(nzInt, nzniFlt), op(div, nzIntConst, var(x))), 
+  type([], [], λ(x, union(nzInt, nzniFlt), op(div, nzIntConst, var(x))), 
        arrow(union(nzInt, nzniFlt),
              union(union(nzniFlt, nzInt),
                    union(nzniFlt, nzInt)))).
 
 test(funDiv0X, [nondet]) :-
-  type([], fun(x, union(nzInt, nzniFlt), op(div, var(x), zeroConst)),
-       T).
+  type([
+    errDivByZeroWithIntNumerator,
+    errDivByZeroWithFloatNumerator
+  ], [], λ(x, union(nzInt, nzniFlt), op(div, var(x), zeroConst)),
+  arrow(union(nzInt, nzniFlt), union(⊥ , ⊥ ))).
 
-%       arrow(union(nzInt, nzniFlt),
-%             union(errDivByZeroWithIntNumerator,
-%                   errDivByZeroWithFloatNumerator))).
+test(plusBot, [nondet]) :-
+  type(
+    [errDivByZeroWithIntNumerator],
+    [],
+    op(plus, op(div, nzIntConst, zeroConst), zeroConst),
+    ⊥
+  ).
+
 
 :- end_tests(types0).
 
