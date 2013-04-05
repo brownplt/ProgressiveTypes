f62cd40cdbc3e2f8e1842e558521e04f4cc353d0
Joe Politz <joe@cs.brown.edu>
2013-04-05 11:51:44 -0400

    small step and (one) test(s)

--- coq/progressives.v
+++ coq/progressives.v
@@ -18,14 +18,18 @@ Inductive w : Type :=
   | app_0 : w
 .
 
+Inductive c : Type :=
+  | div : c
+  | add : c
+.
+
 Inductive expr : Type :=
   | ENum : nat -> expr
   | ELam : id -> expr -> expr
   | EVar : id -> expr
   | EErr : w -> expr
   | EApp : expr -> expr -> expr
-  | EDiv : expr -> expr
-  | EAdd : expr -> expr
+  | EPrim : c -> expr -> expr
 .
 
 Inductive aval : expr -> Prop :=
@@ -38,14 +42,12 @@ Inductive cxt : Type :=
   | EHole : cxt
   | EAppFun : cxt -> expr -> cxt
   | EAppArg : expr -> cxt -> cxt
-  | EDivArg : cxt -> cxt
-  | EAddArg : cxt -> cxt
+  | EPrimArg : c -> cxt -> cxt
 .
 
 Inductive ActExp : expr -> Prop :=
-  | ActDiv : forall n, ActExp (EDiv (ENum n))
-  | ActAdd : forall n, ActExp (EAdd (ENum n))
-  | ActApp : forall e1 e2, ActExp (EApp e1 e2)
+  | ActPrim : forall c e, aval e -> ActExp (EPrim c e)
+  | ActApp : forall e1 e2, aval e1 -> aval e2 -> ActExp (EApp e1 e2)
 .
 
 Inductive EDecomp : expr -> cxt -> expr -> Prop :=
@@ -57,12 +59,9 @@ Inductive EDecomp : expr -> cxt -> expr -> Prop :=
                    aval efun ->
                    EDecomp earg EC ae ->
                    EDecomp (EApp efun earg) (EAppArg efun EC) ae
-  | CxtDivArg : forall EC earg ae,
+  | CxtPrimArg : forall EC c earg ae,
                    EDecomp earg EC ae ->
-                   EDecomp (EDiv earg) (EDivArg EC) ae
-  | CxtAddArg : forall EC earg ae,
-                   EDecomp earg EC ae ->
-                   EDecomp (EAdd earg) (EAddArg EC) ae
+                   EDecomp (EPrim c earg) (EPrimArg c EC) ae
 .
 
 Fixpoint e_plug (c: cxt) (e: expr) : expr :=
@@ -70,8 +69,7 @@ Fixpoint e_plug (c: cxt) (e: expr) : expr :=
     | EHole => e
     | EAppFun c2 e2 => EApp (e_plug c2 e) e2
     | EAppArg e2 c2 => EApp e2 (e_plug c2 e)
-    | EDivArg c2 => EDiv (e_plug c2 e)
-    | EAddArg c2 => EAdd (e_plug c2 e)
+    | EPrimArg op c2 => EPrim op (e_plug c2 e)
   end
 .
 
@@ -80,12 +78,84 @@ Fixpoint subst (x: id) (s: expr) (t: expr) : expr :=
     | ENum n => ENum n
     | ELam x2 e => if (beq_id x x2) then (t) else (ELam x2 (subst x s e)) 
     | EVar x2 => if (beq_id x x2) then s else t
-    | EErr w => EErr w
+    | EErr err_w => EErr err_w
     | EApp e1 e2 =>EApp (subst x s e1) (subst x s e2)
-    | EDiv e => EDiv (subst x s e)
-    | EAdd e => EAdd (subst x s e)
+    | EPrim op_c e => EPrim op_c (subst x s e)
     end
 .
 
-Inductive step : expr -> expr-> Prop :=
+(*
+(** 6. Finally, define the small-step relation.  Just one case
+   suffices for all valid decompositions of expressions! *)
+Reserved Notation " t '/' st '==>a' t' " (at level 40, st at level 39).
+Inductive astep : state -> aexp -> aexp -> Prop :=
+  | A_step : forall st a E aact a', 
+    Adecompose a E aact ->
+      aact / st ==>a a' ->
+      a / st ==>a (aexp_plug E a')
+
+  | AS_Id : forall st i, 
+      AId i / st ==>a ANum (st i)
+  | AS_Plus : forall st n1 n2,
+      APlus (ANum n1) (ANum n2) / st ==>a ANum (n1 + n2)
+  | AS_Minus : forall st n1 n2,
+      (AMinus (ANum n1) (ANum n2)) / st ==>a (ANum (minus n1 n2))
+  | AS_Mult : forall st n1 n2,
+      (AMult (ANum n1) (ANum n2)) / st ==>a (ANum (mult n1 n2))
+
+    where " t '/' st '==>a' t' " := (astep st t t').
+*)
+
+(* thatsthejoke.jpg *)
+Fixpoint int_reciprocal (n : nat) := 0.
+
+Inductive delt : c -> expr -> expr -> Prop :=
+ | DDivZero : delt div (ENum 0) (EErr div_0)
+ | DDivN :
+   forall n, beq_nat n 0 = false ->
+   delt div (ENum n) (ENum (int_reciprocal n))
+ | DDivLam :
+   forall x e,
+   delt div (ELam x e) (EErr div_lam)
+
+ | DAddN :
+   forall n,
+   delt add (ENum n) (ENum (S n))
+ | DAddLam :
+   forall x e,
+   delt add (ELam x e) (EErr add_lam)
 .
+
+Inductive step : expr -> expr -> Prop :=
+ | StepCxt : forall e E ae ae',
+   EDecomp e E ae ->
+   step ae ae' ->
+   step e (e_plug E ae')
+
+ | StepErr : forall e E w,
+   EDecomp e E (EErr w) ->
+   step e (EErr w)
+
+ | StepApp : forall x eb ea,
+   aval ea ->
+   step (EApp (ELam x eb) ea) (subst x ea eb)
+ | StepAppNum : forall n ea,
+   aval ea -> beq_nat n 0 = false ->
+   step (EApp (ENum n) ea) (EErr app_n)
+ | StepApp0 : forall ea,
+   aval ea ->
+   step (EApp (ENum 0) ea) (EErr app_0)
+
+ | StepPrim : forall c ea ea',
+   aval ea ->
+   delt c ea ea' ->
+   step (EPrim c ea) ea'
+.
+
+Example step1 :
+  step (EPrim div (ENum 22)) (ENum 0)
+.
+Proof.
+  apply StepPrim. apply av_num. apply DDivN. reflexivity.
+Qed.
+
